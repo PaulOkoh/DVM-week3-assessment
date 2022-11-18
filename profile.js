207550ee42b04d8ed6fc0ec568f0f625f03cc48a
Paul Okoh <pauldokoh@gmail.com>
2022-11-18 17:01:55 -0600

    add solution for profile

--- profile.js
+++ profile.js
@@ -1,7 +1,17 @@
-let btnsList = document.querySelector("#place");
+let colorBtn = document.querySelector("#color");
+let placeBtn = document.querySelector("#place");
+let ritualBtn = document.querySelector("#ritual");
 
 function alertMessage(event) {
-  console.log(event.target.id);
+  if (event.target.id === "color") {
+    alert(`my favorite color is blue`);
+  } else if (event.target.id === "place") {
+    alert(`my favorite place is Amsterdam`);
+  } else if (event.target.id === "ritual") {
+    alert(`favorite ritual: cup of tea in the morning`);
+  }
 }
 
-btnsList.addEventListener("click", alertMessage);
+colorBtn.addEventListener("click", alertMessage);
+placeBtn.addEventListener("click", alertMessage);
+ritualBtn.addEventListener("click", alertMessage);
